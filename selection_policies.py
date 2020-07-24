124486389c1aff8a4e806305b2db30d49e5e5d69
ericch <ericch@umich.edu>
2020-07-23 23:52:58 -0400

    cleanup

--- selection_policies.py
+++ selection_policies.py
@@ -36,10 +36,10 @@ def select_stochastic(ranking, k):
 	"""
 	Method 2 (stochastic):
 	Selects k subjects in the following manner:
-		- starting at the top-ranked subject (index 0), decide whether to select them based on position bias
-			* we'll use a log discounting click models: 1 / log_2(1 + i)
+		- starting at the top-ranked subject (index 0), select them with probability based on position bias
+			* we'll use a log discounting "click model": 1 / log_2(1 + i) for i = 1, 2, 3, ...
 		- if not selected, move down a rank and "click on" the next subject
-		- stop when we have k selected or reach the bottom of the list
+		- stop when we have k selected subjects or reach the bottom of the list
 	Each selected individual then succeeds with probability of success equal to relevance.
 	"""
 	
