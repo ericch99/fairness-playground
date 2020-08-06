c6b91bd3fe1061e4c5ad297c25fcca74f9529186
meehann <meehann@umich.edu>
2020-08-06 12:15:30 -0400

    new experiment

--- heatmaps.py
+++ heatmaps.py
@@ -7,22 +7,25 @@ from distributions import *
 from metrics import *
 from ranking_policies import *
 from selection_policies import *
+from gen_input import generate_input
+from simulation import Simulation
 
 sns.set(style='darkgrid')
 
 
 def main():
     # initialize constants =================================================================
-    NEUTRAL = 0
-    SUCCESSES = [0.25, 0.5, 0.75, 1]
-    FAILS = [-0.25, -0.5, -0.75, -1]
-    matrix = np.zeros(shape=(len(SUCCESSES), len(FAILS)))
+    # NEUTRAL = 0
+    MEANS_A = [0, 0.25, 0.5, 0.75, 1]
+    MEANS_B = [0, -0.25, -0.5, -0.75, -1]
+    matrix = np.zeros(shape=(len(MEANS_A), len(MEANS_B)))
 
     # run simulation once for each combination of r and c ==================================
-    for r in SUCCESSES:
-        for c in FAILS:
-            A, B, sim, _ = generate_input()
-            penalty = {'pos': r, 'neg': c, 'non': NEUTRAL}
+    for r in range(len(MEANS_A)):
+        for c in range(len(MEANS_B)):
+            A, B, sim, penalty = generate_input()
+            A = {'mean': MEANS_A[r], 'var': 1.0, 'prob': 0.7}
+            B = {'mean': MEANS_B[c], 'var': 1.0, 'prob': 0.3}
 
             s = Simulation(A, B, sim, penalty)
             
@@ -37,12 +40,18 @@ def main():
             plt.cla()
             plt.plot(np.arange(10), mean_a, color='C2', label=f'Group A mean')
             plt.plot(np.arange(10), mean_b, color='C0', label=f'Group B mean')
-            plt.savefig(f'figures/heatmaps/means:{r},{c}.pdf', dpi=300)
+            plt.ylabel('mean')
+            plt.xlabel('iteration')
+            plt.savefig(f'figures/heatmaps/means:{MEANS_A[r]},{MEANS_B[c]}.pdf', dpi=300)
 
     # plot heatmap of difference in means ================================================== 
     plt.cla()
-    sns.heatmap(matrix)
-    plt.savefig(f'figures/heatmaps/feedback_coefficients.pdf', dpi=300)
+    sns.heatmap(matrix, cmap="Blues")
+    plt.xticks(MEANS_A, MEANS_A)
+    plt.yticks(MEANS_B, MEANS_B)
+    plt.ylabel('group b means')
+    plt.xlabel('group a means')
+    plt.savefig(f'figures/heatmaps/mean_differences.pdf', dpi=300)
 
 
 if __name__ == '__main__':
